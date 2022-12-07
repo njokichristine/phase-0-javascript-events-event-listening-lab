e727ad232913331cc8e76c0929ffa0994fe19f76
njokichristine <christinenjoki6900@gmail.com>
2022-12-07 08:34:58 +0300

    done

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
-function addingEventListener(){
-    const input =  document.getElementById("button");
-function alert_me()
-{
-    alert("I was clicked");
-}
-input.addEventListener("click",alert_me);
-}
 
+function addingEventListener() {
+    function addingEventListener(){
+        const input =  document.getElementById("button");
+    function alert_me()
+    {
+        alert("I was clicked");
+    }
+    input.addEventListener("click",alert_me);
+}
